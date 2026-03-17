c9893914102a24a2238e4b8fbe85b4404cf88c3b
hagar-dolev <hagar.dolev@gmail.com>
2026-03-17 19:27:05 +0200

    removing home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
 import { GuidesIndex } from './pages/GuidesIndex';
 import { GuidePage } from './pages/GuidePage';
 import { Journalism } from './pages/Journalism';
